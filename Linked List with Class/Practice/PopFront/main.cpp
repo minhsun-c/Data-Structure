93e8887d06ca330e126182e3204a2b73600c3233
minhsun-c <107985606+minhsun-c@users.noreply.github.com>
2022-07-16 22:51:36 +0800

    Create main.cpp

---
+++ Linked List with Class/Practice/PopFront/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    
+    /* constructor */
+        Node ();
+        Node (int);
+        Node (Node &);
+    /* end constructor */
+};
+
+class List
+{
+public:
+    Node *head;
+    
+    List();
+    
+    void pushFront(int);
+    void print();
+    int popFront();
+};
+
+void List::print()
+{
+    // Finish this function
+}
+
+int List::popFront()
+{
+    // Finish this function
+}
+
+int main()
+{
+    List *list = new List;
+    int len;
+    std::cin >> len;
+    for (int i=0; i<len; i++)
+    {
+        int input;
+        std::cin >> input;
+        list -> pushFront(input);
+    }
+    while (list -> head != NULL)
+    {
+        list -> print();
+        list -> popFront();
+    }
+}
+
+Node::Node ()
+{
+    this -> data = 0;
+    this -> next = NULL;
+}
+Node::Node (int _data)
+{
+    this -> data = _data;
+    this -> next = NULL;
+}
+Node::Node (Node &node)
+{
+    this -> data = node.data;
+    this -> next = node.next;
+}
+
+List::List() { this -> head = NULL; }
+
+void List::pushFront(int _data)
+{
+    Node *tmp = this -> head;
+    Node *newNode = new Node(_data);
+    this -> head = newNode;
+    newNode -> next = tmp;
+}
